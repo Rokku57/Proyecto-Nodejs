3f0fde0398c4af8521aa39e72b10885df742faf5
Rokku57 <cancer123david@gmail.com>
2023-03-07 23:00:46 -0500

    07 en proceso

--- conceptos/monohilo.js
+++ conceptos/monohilo.js
@@ -1,5 +1,15 @@
 console.log('Hola mundo');
-
+let i=0
 setInterval(function(){
-    console.log('Sigo activo');
+    //console.log('Sigo activo');
+    console.log(i);
+    i++;
+    //if (i===5) {
+    //    console.log('Forzamos error')
+   //     var a=3 +z;
+   // } else {
+        
+   // }
 },1000);
+
+console.log('Segunda instrucion');
